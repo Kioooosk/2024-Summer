b06e2f8ee9f6739fe2204dfb34632c8e68d6dce0
yooniicode <estelle0329@ewha.ac.kr>
2024-08-09 22:53:11 +0900

    feat: scene1-3 수정

--- frontend/src/component/step1-3.js
+++ frontend/src/component/step1-3.js
@@ -4,15 +4,29 @@ import './StepStyles.css';
 function StepOneThree() {
     return (
         <div className="container">
-            <div className="header-container">
-                <h1 className="header">모두 다 완료되었어요 :)</h1>
+            {/* 최상단 진회색 박스 */}
+            <div className="top-bar">
+                병원 키오스크 연습용 웹, 홀로서기
             </div>
-            <div className="sub-header">외래 도착 알림 전용 키오스크</div>
-            <div className="queue-display">
-                OO 환자분의 대기 순번은 <br />
-                <strong className="queue-number">3번</strong>
+
+            {/* 키오스크 모양 구조 */}
+            <div className="kiosk-wrapper">
+                <div className="kiosk-base">
+                    <div className="kiosk">
+                        <div className="kiosk-screen">
+
+                            <div className="sub-header">외래 도착 알림 전용 키오스크</div>
+                            <div className="queue-display">
+                                OO 환자분의 대기 순번은 <br/>
+                                <strong className="queue-number">3번</strong>
+                            </div>
+                            <div className="footer">진료실로 들어가실 때 이름을 불러드립니다!</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="footer">진료실로 들어가실 때 이름을 불러드립니다!</div>
+
+            {/* 하단의 '시작 화면으로 돌아가기' 텍스트 */}
             <div className="start-over">
                 시작 화면으로 돌아가기
             </div>
