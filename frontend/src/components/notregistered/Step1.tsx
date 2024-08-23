97399fa25614e325287492c6f686225aab408f63
smallsilverwest <smallsilverwest@ewha.ac.kr>
2024-08-24 02:46:21 +0900

    chore: 중앙정렬

--- frontend/src/components/notregistered/Step1.tsx
+++ frontend/src/components/notregistered/Step1.tsx
@@ -16,8 +16,8 @@ const Step1: React.FC = () => {
 
     return (
         <div>
-            <p style={{ fontSize: '20px', margin: '0' }}>환자분의 대기 인원수는</p>
-            <p style={{ fontSize: '80px' }}>{randomNumber}</p>
+            <p style={{ fontSize: '20px', margin: '0', textAlign: 'center' }}>환자분의 대기 인원수는</p>
+            <p style={{ fontSize: '80px', textAlign: 'center' }}>{randomNumber}</p>
             <br />
             <p style={{ fontSize: '18px', margin: '0' }}>접수하시려면 아래 버튼을 눌러주세요</p>
             <br />
