a1c3576ce9672a770f7b4abcc12aa87f89a4961d
yooniicode <estelle0329@ewha.ac.kr>
2024-08-23 17:53:23 +0900

    style: namefield, 화살표 이미지 루트 수정 등

---
+++ frontend/src/components/certificate/NameField.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface NameFieldProps {
+  name: string;
+  setName: (name: string) => void;
+}
+
+const StyledInput = styled.input`
+    font-size: 20px;
+    padding: 10px;
+    width: 80%;
+    box-sizing: border-box;
+
+    &::placeholder {
+        font-family: Suite, sans-serif;
+        font-size: 18px;
+        color: #888;
+    }
+`;
+
+const NameField: React.FC<NameFieldProps> = ({ name, setName }) => {
+  return (
+    <StyledInput
+      type="text"
+      placeholder="이름을 입력하세요"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    />
+  );
+};
+
+export default NameField;
